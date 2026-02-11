63fb0b8078be362a347bc71fc431168bbf23cb51
unnati1805 <unnatig1805@gmail.com>
2026-02-11 19:21:55 +0530

    basics of java
    Basics of Java

---
+++ LinearSearch.java
@@ -0,0 +1,16 @@
+public class LinearSearch {
+    public static int linearSearch(int arr[], int key){
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            if(arr[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[] = {5,2,6,1,8};
+        int key = 6;
+        System.out.println(linearSearch(arr, key));
+    }
+}
